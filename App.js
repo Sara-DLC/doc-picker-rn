c2fda7d2636c4a7746bc2cdac05ed1d852737afc
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-02-27 13:46:41 -0500

    convert to fun component
    Co-authored-by: Sara De La Cruz <54650844+Sara-DLC@users.noreply.github.com>

--- App.js
+++ App.js
@@ -1,82 +1,123 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import * as DocumentPicker from 'expo-document-picker';
-import { 
-StyleSheet, 
-Text, 
-TouchableOpacity, 
-View } from 'react-native';
+import {
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View,
+} from 'react-native';
 
 
-export default class App extends Component {
+export default function App(props) {
+    const [ file, setFile ] = useState({});
 
-constructor(props) {
-super(props);
-this.state = {
-    singleFileOBJ: '',
-};
+    async function pickFile() {
+        try {
+            const response = await DocumentPicker.getDocumentAsync({
+                type: '*/*',
+                copyToCacheDirectory: false,
+                multiple: false,
+            });
+
+            setFile(response);
+        }
+        catch (err) {
+            console.log(err);
+        }
+    }
+
+    return (
+        <View style={styles.MainContainer}>
+
+            <Text style={styles.text}>
+                File Name: {file.name ? file.name : ''}
+            </Text>
+
+            <TouchableOpacity
+                activeOpacity={0.5}
+                style={styles.button}
+                onPress={pickFile}
+            >
+                <Text style={styles.buttonText}>
+                    Click Here To Pick File
+                </Text>
+            </TouchableOpacity>
+
+        </View>
+    );
 }
 
-async SingleFilePicker() {
-try {
-    const res = await DocumentPicker.getDocumentAsync({
-    type: "*/* ",
-    copyToCacheDirectory: false,
-    multiple: false
-    });
+// class App extends Component {
 
-    this.setState({ singleFileOBJ: res });
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             singleFileOBJ: '',
+//         };
+//     }
 
-} catch (err) {
-    console.log(err)
-}
-}
+//     async SingleFilePicker() {
+//         try {
+//             const res = await DocumentPicker.getDocumentAsync({
+//                 type: '*/* ',
+//                 copyToCacheDirectory: false,
+//                 multiple: false,
+//             });
 
-render() {
-return (
-    <View style={styles.MainContainer}>
+//             this.setState({ singleFileOBJ: res });
 
-    <Text style={styles.text}>
-        File Name: {this.state.singleFileOBJ.name ? this.state.singleFileOBJ.name : ''}
-    </Text>
+//         }
+//         catch (err) {
+//             console.log(err);
+//         }
+//     }
 
-    <TouchableOpacity
-        activeOpacity={0.5}
-        style={styles.button}
-        onPress={this.SingleFilePicker.bind(this)}>
-        <Text style={styles.buttonText}>
-        Click Here To Pick File
-        </Text>
-    </TouchableOpacity>
-    </View>
-);
-}
-}
+//     render() {
+//         return (
+//             <View style={styles.MainContainer}>
+
+//                 <Text style={styles.text}>
+//         File Name: {this.state.singleFileOBJ.name ? this.state.singleFileOBJ.name : ''}
+//                 </Text>
+
+//                 <TouchableOpacity
+//                     activeOpacity={0.5}
+//                     style={styles.button}
+//                     onPress={this.SingleFilePicker.bind(this)}>
+//                     <Text style={styles.buttonText}>
+//         Click Here To Pick File
+//                     </Text>
+//                 </TouchableOpacity>
+//             </View>
+//         );
+//     }
+// }
 
 const styles = StyleSheet.create({
-MainContainer: {
-flex: 1,
-backgroundColor: '#fff',
-padding: 16,
-justifyContent: 'center',
-},
+    MainContainer: {
+        flex: 1,
+        backgroundColor: '#fff',
+        padding: 16,
+        justifyContent: 'center',
+    },
 
-button: {
-width: '100%',
-backgroundColor: '#0091EA',
-borderRadius:9,
-},
+    button: {
+        width: '100%',
+        backgroundColor: '#0091EA',
+        borderRadius: 9,
+    },
 
-buttonText: {
-color: '#fff',
-fontSize: 21,
-padding: 10,
-textAlign: 'center'
-},
+    buttonText: {
+        color: '#fff',
+        fontSize: 21,
+        padding: 10,
+        textAlign: 'center',
+    },
 
-text: {
-color: '#000',
-fontSize: 16,
-padding: 10,
-textAlign: 'left'
-},
+    text: {
+        color: '#000',
+        fontSize: 16,
+        padding: 10,
+        textAlign: 'left',
+    },
 });
